2a6729e0e3450095f2ae1afafedc0ae154f13b12
Kirill Solovyov <andrey@MacBook-Pro-Kirill.local>
2017-04-01 12:59:00 +0300

    Initial Commit

---
+++ Dharm/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Dharm
+//
+//  Created by Кирилл on 01.04.17.
+//  Copyright © 2017 Kirill Solovov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Dharm/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Dharm
+//
+//  Created by Кирилл on 01.04.17.
+//  Copyright © 2017 Kirill Solovov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
