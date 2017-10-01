66538da7937d7c09e43683efdba88d394f21edb3
Kirill Solovyov <kirill_solovyov@yahoo.com>
2017-10-01 17:02:34 +0300

    AlertHelper added

---
+++ Dharm/SKAlertHelper.m
@@ -0,0 +1,57 @@
+//
+//  SKAlertHelper.m
+//  Dharm
+//
+//  Created by Kirill Solovyov on 01.10.17.
+//  Copyright © 2017 Kirill Solovyov. All rights reserved.
+//
+
+#import "SKAlertHelper.h"
+
+@implementation SKAlertHelper
+
+- (void) showCantEnterCodeAlert {
+    
+    NSString *alertTitle = NSLocalizedString(@"ALERTERROR", nil);
+    NSString *alertMessage = NSLocalizedString(@"ALERTMESSAGE", nil);
+    NSString *alertCancel = NSLocalizedString(@"OK", nil);
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:alertTitle
+                                                                   message:alertMessage
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:alertCancel
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+    
+    [alert addAction:cancelAction];
+    [self.delegate presentViewController:alert animated:YES completion:nil];
+    
+}
+
+- (void) showResetStoryAlertWithOkHandler:(void(^)(UIAlertAction * _Nonnull action)) handler {
+    
+    NSString *alertTitle = NSLocalizedString(@"RESETTITLE", nil);
+    NSString *alertMessage = NSLocalizedString(@"RESETMESSAGE", nil);
+    NSString *okTitle = NSLocalizedString(@"OK", nil);
+    NSString *cancelTitle = NSLocalizedString(@"CANCEL", nil);
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:alertTitle
+                                                                   message:alertMessage
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:okTitle
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:handler];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:cancelTitle
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:nil];
+    
+    [alert addAction:okAction];
+    [alert addAction:cancelAction];
+    
+    [self.delegate presentViewController:alert animated:YES completion:nil];
+}
+
+@end
